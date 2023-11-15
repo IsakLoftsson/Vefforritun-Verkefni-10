198988ff05bf761ebb645e7cb863d053a7c9e127
Ísak <isl5@hi.is>
2023-11-15 17:12:35 +0000

    fyrsta síða done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 // import logo from './logo-mr-man.png';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [animationDuration, setAnimationDuration] = useState(10);
+
+  const handleSliderChange = (event) => {
+    setAnimationDuration(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={process.env.PUBLIC_URL + '/logo-mr-man.png'} className="App-logo" alt="logo" />
-        <p>
-          Hey kids, this is my code :)
-        </p>
+        <img
+          src={process.env.PUBLIC_URL + '/logo-mr-man.png'}
+          className="App-logo"
+          alt="logo"
+          style={{
+            animation: `App-logo-spin infinite ${animationDuration}s linear`,
+          }}
+        />
+        <h1>Hæ kennari, þetta er ég, Ísak :^) </h1> <p>Ég notaði React fyrir þetta! Sjáðu hvað ég get farið hratt! </p>
+        <input
+          type="range"
+          min="1"
+          max="20"
+          value={animationDuration}
+          onChange={handleSliderChange}
+        />
+        <span>{`${21-animationDuration} Km/klst`}s</span> {}
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="./location.html"
           rel="noopener noreferrer"
         >
-          Learn React
+          Smelltu hér til að halda áfram
         </a>
       </header>
     </div>
