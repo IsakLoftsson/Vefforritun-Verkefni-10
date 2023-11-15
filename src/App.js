93b2342cdcfbc6a6732e1ccc9e1c6b7db9d9a0db
Ísak <isl5@hi.is>
2023-11-15 14:23:21 +0000

    dope image update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo-mr-man.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={process.env.PUBLIC_URL + '/logo-mr-man.png'} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey kids, this is my code :)
         </p>
         <a
           className="App-link"
