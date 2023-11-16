d776ed89df4706c07837a47806d2d50afa3ebb42
Ísak <isl5@hi.is>
2023-11-16 09:57:05 +0000

    Version 2.0

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 Ég notaði react til að setja upp projectið, með því að keyra Create-react-app.
 
+### HTML Drag and Drop API
+
+Ég notaði HTML Drag and Drop API til þessa að geta drag and droppað myndinni af mér sem snýst.
+
 ### Keyrsla
 
 Nota `npm start` til að opna síðuna.
--- src/App.js
+++ src/App.js
@@ -1,82 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-//asdfasdfasdfasdf
-/*
-const draggableElements = document.querySelectorAll(".draggable");
-const droppableElements = document.querySelectorAll(".droppable");
-console.log('draggableElements', draggableElements);
-console.log('droppableElements', droppableElements);
 
-draggableElements.forEach(elem => {
-  elem.addEventListener("dragstart", dragStart); // Fires as soon as the user starts dragging an item - This is where we can define the drag data
-  // elem.addEventListener("drag", drag); // Fires when a dragged item (element or text selection) is dragged
-  // elem.addEventListener("dragend", dragEnd); // Fires when a drag operation ends (such as releasing a mouse button or hitting the Esc key) - After the dragend event, the drag and drop operation is complete
-});
-
-droppableElements.forEach(elem => {
-  elem.addEventListener("dragenter", dragEnter); // Fires when a dragged item enters a valid drop target
-  elem.addEventListener("dragover", dragOver); // Fires when a dragged item is being dragged over a valid drop target, repeatedly while the draggable item is within the drop zone
-  elem.addEventListener("dragleave", dragLeave); // Fires when a dragged item leaves a valid drop target
-  elem.addEventListener("drop", drop); // Fires when an item is dropped on a valid drop target
-});
-*/
-
+// Þegar ég byrja að draga draggable
 function dragStart(event) {
   //console.log('dragStart event', event.target.classList);
   event.dataTransfer.setData("text", event.target.id); // or "text/plain" but just "text" would also be fine since we are not setting any other type/format for data value
 }
 
+// Þegar ég fer inn á svæði droppable með draggable 
 function dragEnter(event) {
-  if(!event.target.classList.contains("dropped")) {
-    //console.log('dragEnter event', event.target.classList);
+  if(!event.target.classList.contains("dropped") || event.target.classList.contains("droppable")) {
+    // console.log('dragEnter event', event.target.classList);
+    event.target.classList.remove("dropped");
     event.target.classList.add("droppable-hover");
-    //console.log('dragEnter event 2', event.target.classList);
+    // console.log('dragEnter event 2', event.target.classList);
   }
 }
 
-let i = 0;
+// Þegar ég er yfir svæði droppable með draggable
 function dragOver(event) {
-  i=i+1;
   if(!event.target.classList.contains("dropped")) {
     event.preventDefault(); // Prevent default to allow drop
     //console.log('dragOver event number ',i, event.target.classList);
   }
 }
 
+// Þegar ég fer út af svæði droppable með draggable
 function dragLeave(event) {
   if(!event.target.classList.contains("dropped")) {
     event.target.classList.remove("droppable-hover");
     //console.log('dragLeave event', event.target.classList);
   }
+  if(event.target.classList.contains("dropped")) {
+    event.target.classList.remove("dropped");
+    //console.log('dragLeave event', event.target.classList);
+  }
 }
 
+// Þegar ég sleppi draggable á svæði droppable
 function drop(event) {
-  console.log('drop event 1', event.target.classList);
-
+  //console.log('drop event 1', event.target.classList);
   event.preventDefault(); // This is in order to prevent the browser default handling of the data
   event.target.classList.remove("droppable-hover");
-  console.log('drop event 2', event.target.classList);
-
-  // useless i think // const draggableElementData = event.dataTransfer.getData("text"); // Get the dragged data. This method will return any data that was set to the same type in the setData() method
+  // console.log('drop event 2', event.target.classList);
   const draggableElementData = document.querySelector(".draggable")
-  console.log('image draggable', draggableElementData);
-
-  // const draggableElement = document.getElementById(draggableElementData);
-  event.target.classList.add("dropped");
-  console.log('drop event 4', event.target.classList);
-
-  //event.target.style.backgroundColor = window.getComputedStyle(draggableElement).color;
+  // console.log('image draggable', draggableElementData);
+  // console.log('drop event 4', event.target.classList);
   console.log('image draggable before change', draggableElementData);
   console.log('draggableElementData.classList: ', draggableElementData.classList);
   draggableElementData.classList.add("dragged");
   console.log('draggableElementData.classList 2: ', draggableElementData.classList);
   console.log('image draggable after change', draggableElementData);
-
-  console.log('drop event 5', event.target.classList);
-
-  // draggableElementData.setAttribute("draggable", "false");
-  // event.target.insertAdjacentHTML("afterbegin", `<i class="fas fa-${draggableElementData}"></i>`)
-  event.target.appendChild(draggableElementData);
+  // console.log('drop event 5', event.target.classList);
+  if (!event.target.classList.contains("dropped") && !event.target.classList.contains("draggable")) {
+    event.target.appendChild(draggableElementData);
+  }
+  event.target.classList.add("dropped");
 }
 
 function App() {
@@ -90,16 +69,16 @@ function App() {
     console.log('droppableElements', droppableElements);
 
     draggableElements.forEach(elem => {
-      elem.addEventListener("dragstart", dragStart); // Fires as soon as the user starts dragging an item - This is where we can define the drag data
+      elem.addEventListener("dragstart", dragStart);
       // elem.addEventListener("drag", drag); // Fires when a dragged item (element or text selection) is dragged
       // elem.addEventListener("dragend", dragEnd); // Fires when a drag operation ends (such as releasing a mouse button or hitting the Esc key) - After the dragend event, the drag and drop operation is complete
     });
 
     droppableElements.forEach(elem => {
-      elem.addEventListener("dragenter", dragEnter); // Fires when a dragged item enters a valid drop target
-      elem.addEventListener("dragover", dragOver); // Fires when a dragged item is being dragged over a valid drop target, repeatedly while the draggable item is within the drop zone
-      elem.addEventListener("dragleave", dragLeave); // Fires when a dragged item leaves a valid drop target
-      elem.addEventListener("drop", drop); // Fires when an item is dropped on a valid drop target
+      elem.addEventListener("dragenter", dragEnter);
+      elem.addEventListener("dragover", dragOver);
+      elem.addEventListener("dragleave", dragLeave);
+      elem.addEventListener("drop", drop);
     });
 
   }, []);
@@ -115,7 +94,7 @@ function App() {
       <header className="App-header">
       <section className="droppable-elements">
           <div className="droppable" ></div>
-          <div className="droppable" ><span>
+          <div className="droppable dropped" ><span>
             <img
               src={process.env.PUBLIC_URL + '/logo-mr-man.png'}
               className="draggable"
@@ -128,8 +107,9 @@ function App() {
           </span></div>
           <div className="droppable"><span></span></div>
         </section>
-        <h1> Hæ kennari, þetta er ég, Ísak :^) </h1> 
-        <p>Ég notaði React fyrir verkefnið og HTML Drag & Drop! Sjáðu hvað ég get farið hratt! Þú getur líka fært mig í annað box!</p>
+        <h1> Hæ, þetta er ég, Ísak :^) </h1> 
+        <h2> Sjáðu hvað ég get farið hratt! Þú getur líka fært mig í annað box!</h2>
+        <p>Ég notaði React til að setja upp verkefnið og HTML Drag & Drop til að leika mér! </p>
         <input  
           type="range"
           min="1"
@@ -138,13 +118,6 @@ function App() {
           onChange={handleSliderChange}
         />
         <span>{`${21-animationDuration} Km/klst`}s</span> {}
-        <a
-          className="App-link"
-          href="./location.html"
-          rel="noopener noreferrer"
-        >
-          Ekki smella!!!
-        </a>
       </header>
     </div>
   );
